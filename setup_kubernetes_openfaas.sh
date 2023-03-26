0e0f439599d84d4f9a622d24dd50524ef520b907
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2023-03-25 22:48:15 -0500

    Changes to Kubernetes setup script

--- setup_kubernetes_openfaas.sh
+++ setup_kubernetes_openfaas.sh
@@ -110,11 +110,11 @@ metadata:
 handler: runsc
 EOF
         sudo apt-get update
-	sudo apt-get install ca-certificates curl gnupg lsb-release
+	sudo apt-get install -y ca-certificates curl gnupg lsb-release
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt-get update
-	sudo apt-get install docker-ce docker-ce-cli
+	sudo apt-get install -y docker-ce docker-ce-cli
 
 	echo "#################################"
 	echo "Installing OpenFaas"
