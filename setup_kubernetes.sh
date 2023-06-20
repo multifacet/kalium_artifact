9197e5c061df3582b55097eeea3e233856d43f02
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2023-06-20 16:58:18 -0500

    Update Setup Script

--- setup_kubernetes.sh
+++ setup_kubernetes.sh
@@ -27,17 +27,17 @@ EOF
 sudo sysctl --system
 
 sudo apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common apparmor
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
 
-sudo apt-get update && sudo apt-get remove containerd.io && sudo apt-get install -y containerd.io=1.6.12-1
+sudo apt-get update && sudo apt-get remove -y containerd.io && sudo apt-get install -y containerd.io=1.6.12-1
 
 sudo mkdir -p /etc/containerd
-sudo containerd config default > /etc/containerd/config.toml
+containerd config default | sudo tee /etc/containerd/config.toml
 
 sudo systemctl restart containerd
 
